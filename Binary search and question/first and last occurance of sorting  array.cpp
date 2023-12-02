ae2b2f1fa7f90e2bcd2680bb4f0ee09fdf71e059
yogesh baghel <139314033+Yogesh-Baghel12@users.noreply.github.com>
2023-12-02 23:41:31 +0530

    edit in code

--- Binary search and question/first and last occurance of sorting  array.cpp
+++ Binary search and question/first and last occurance of sorting  array.cpp
@@ -16,6 +16,8 @@ int  Search(int arr[],int n,int key){
         else
             end=mid-1;
     }
+    start=0;
+    end=n-1;
     while (start<=end) {
         int mid = start+(end-start/2);
         if(arr[mid]==key){
